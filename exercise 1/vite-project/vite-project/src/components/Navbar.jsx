1eb60ad676d6fa47165473a6d002a3b79094a819
GitHub User <user@github.com>
2026-04-27 11:04:43 +0530

    vnrvjiet

---
+++ exercise 1/vite-project/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">VNRVJIET</Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/courses">Courses</Link></li>
+        <li><Link to="/faculty">Faculty</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
